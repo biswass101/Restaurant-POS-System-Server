303307f9bab8d97fdbb6bafe5fbe2a0013af7739
biswass101 <biswassnaeemcse@gmail.com>
2025-03-06 10:50:14 +0600

    completed table api's

--- controllers/order.Controller.js
+++ controllers/order.Controller.js
@@ -1,5 +1,6 @@
 const createHttpError = require('http-errors');
 const Order = require('../models/order.Model')
+const mongoose = require('mongoose');
 
 const addOrder = async(req, res, next) => {
     try {
@@ -12,7 +13,15 @@ const addOrder = async(req, res, next) => {
 }
 const getOrder = async(req, res, next) => {
     try {
-        const order = await Order.findById(req.params.id);
+
+        const {id} = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            const error = createHttpError(404, "Invalid Id")
+            return next(error);
+        }
+
+        const order = await Order.findById(id);
         if(!order) {
             const error = createHttpError(404, "Order not found")
             return next(error);
@@ -35,8 +44,16 @@ const getOrders = async(req, res, next) => {
 const updateOrder = async(req, res, next) => {
     try {
         const { orderStatus } = req.body;
+
+        const {id} = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            const error = createHttpError(404, "Invalid Id")
+            return next(error);
+        }
+
         const order = await Order.findByIdAndUpdate(
-            req.params.id,
+            id,
             {orderStatus},
             {new: true}
         );
